9534d0cbdbd7ce2cbfe27b9dc786079721dddbe6
MUHAMMED AFZAL <vamuhammedafzal@gmail.com>
2024-10-08 18:04:55 +0530

    Create sample_info.py

---
+++ sample_info.py
@@ -0,0 +1,22 @@
+# Bot information form star galaxy movies 
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = 'enter your api hash for exp 1234abcdef'
+BOT_TOKEN = 'enter your bot token 123456789'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [6075373591, 'admins', 6075373591]
+CHANNELS = [-1002126066365,-1002126066365, 'channelusername']
+AUTH_USERS = [6075373591]
+AUTH_CHANNEL = None
+
+# this is MongoDB information
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
